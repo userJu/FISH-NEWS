a53d8ea5333f169a73a60ea64b1e4c07bd31af72
JuhyunLim <87933367+userJu@users.noreply.github.com>
2021-10-12 18:06:09 +0900

    Js breaking right btn active
    고생했다주현아..

---
+++ main.js
@@ -0,0 +1,59 @@
+// Breaking button click event
+const leftBtn = document.querySelector('.breakings_leftBtn')
+const rightBtn = document.querySelector('.breakings_rightBtn')
+const breaking = document.querySelector('.breaking')
+
+let i = 0
+
+leftBtn.addEventListener('click', (e)=>{
+    console.log('move to left')
+})
+rightBtn.addEventListener('click',(e)=>{
+    console.log('move to right')
+    i++
+
+if(i > 3){
+    or = i % 4
+    deleteBreaking()
+    changeBreaking(or)
+} else {
+    or = i
+    deleteBreaking()
+    changeBreaking(or)
+}
+})
+
+imgArray = ["1.png", "2.png", "3.jpg", "4.jpg"]
+txtArray =[
+    `Gilderoy Lockhart, six solid months at the top of the best seller list was in Flourish and Blotts yesterday signing his new book "Magical Me". The bookstore was crammed full of admiring fans most of which were women. Gilderoy Lockhart Wizard Extraordinaire, bewitches and bedazzles us yet again with another toe-nail-bitting account of his heroic encounters with the dark side. Wrestled with werewolves, defied demons, battled bests, Gilderoy's bewildering bravery never ceases to amaze in this magical masterpiece.[10]`,
+    "22",
+    "33",
+    "44",
+]
+
+// Add more breakings
+function changeBreaking (or){
+    const image = document.createElement("img")
+    const breakingInfo = document.querySelector(".breaking__info")
+    const span = document.createElement('span')
+    const h2 =document.createElement('h2')
+    
+    image.classList ="breaking__img"
+    breaking.prepend(image)
+    image.src = `images/breaking/breaking${imgArray[or]}`
+
+    breakingInfo.prepend(span)
+    span.innerText = txtArray[or]
+    span.prepend(h2)
+    h2.innerText = span.innerText.substr(0,1)
+}
+changeBreaking(0)
+
+
+// delete breakings
+function deleteBreaking(){
+    const image = document.querySelector(".breaking__img")
+    const span = breaking.querySelector('span')
+    image.remove()
+    span.remove()
+}
